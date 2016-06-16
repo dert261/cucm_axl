e2512fa4e5468097e9c7b05308c3a4607f7bfab2
Vladimir Bozhenkov <v.bozhenkov@obelisk-voip.ru>
2016-06-16 14:16:57 +0300

    Remove not needed code

--- src/main/java/ru/obelisk/cucmaxl/config/web/AppConfig.java
+++ src/main/java/ru/obelisk/cucmaxl/config/web/AppConfig.java
@@ -1,25 +1,14 @@
 package ru.obelisk.cucmaxl.config.web;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
-
 import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.accept.ContentNegotiationManager;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 
 @Configuration 
 @EnableWebMvc
@@ -43,37 +32,6 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		}
 	}
 	
-	/**
-	*  Total customization - see below for explanation.
-	*/
-/*	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-			
-		configurer.favorPathExtension(true).
-	    	favorParameter(false).
-	        //parameterName("mediaType").
-	        ignoreAcceptHeader(true).
-	        useJaf(false).
-	        defaultContentType(MediaType.APPLICATION_JSON).
-	        mediaType("xml", MediaType.APPLICATION_XML).
-	        mediaType("json", MediaType.APPLICATION_JSON).
-	        mediaType("html", MediaType.TEXT_HTML);
-	}
-	
-/*	@Autowired
-	private ThymeleafConfig thymeleafConfig;
-	
-/*	@Bean
-	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
-	    // Define the view resolvers
-	    List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
-	    resolvers.add(thymeleafConfig.thymeleafViewResolver());
-	    ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
-	    resolver.setViewResolvers(resolvers);
-	    resolver.setContentNegotiationManager(manager);
-	    return resolver;
-	}
-*/	
 	@PostConstruct
 	public void setEnviroment(){
 		Properties systemProps = System.getProperties();
--- src/main/java/ru/obelisk/cucmaxl/web/controllers/LoginController.java
+++ src/main/java/ru/obelisk/cucmaxl/web/controllers/LoginController.java
@@ -1,27 +1,19 @@
 package ru.obelisk.cucmaxl.web.controllers;
 
 import java.io.IOException;
-import java.util.Enumeration;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import lombok.extern.log4j.Log4j2;
-
 @Controller
-@Log4j2
 public class LoginController {
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(ModelMap model, HttpServletRequest request) {
-		
 		return "login";
     }
 
